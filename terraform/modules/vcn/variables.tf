d8ac541e444604c832a4e4d280f86da87da98a7b
Gyanendra Veeru <gyaveeru@publicisgroupe.net>
2022-06-20 00:17:16 +0530

    FIrst Setup

---
+++ terraform/modules/vcn/variables.tf
@@ -0,0 +1,8 @@
+variable "vcn_cidr_block" {
+  type = string
+  default = "10.10.30.0/24"
+}
+
+variable "compartment_id" {
+  type = string
+}
---
+++ terraform/modules/vcn/vcn.tf
@@ -0,0 +1,5 @@
+resource "oci_core_vcn" "test_vcn" {
+    #Required
+    compartment_id = var.compartment_id
+    cidr_block = var.vcn_cidr_block
+}
