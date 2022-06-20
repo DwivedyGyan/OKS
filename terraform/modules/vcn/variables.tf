1ad2607c1f138687785f3898db1b6b5044cf3266
Gyanendra Veeru <gyaveeru@publicisgroupe.net>
2022-06-20 11:18:40 +0530

    First Setup

--- terraform/modules/vcn/variables.tf
+++ terraform/modules/vcn/variables.tf
@@ -6,3 +6,7 @@ variable "vcn_cidr_block" {
 variable "compartment_id" {
   type = string
 }
+
+variable "vcn_display_name" {
+  type = string
+}
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,3 +2,11 @@ variable "compartment_id" {
   type = string
   default = "ocid1.tenancy.oc1..aaaaaaaa3kxlkkmpaaar5vamcwcpl5gxiyxtiktgxyav7s5whuxbdmjbyeia"
 }
+
+variable "cluster_kubernetes_version" {
+  type = string  
+}
+
+variable "cluster_name" {
+  type = string
+}
