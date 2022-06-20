dd73985438b775257a4ac8b46920e87d17b39798
Gyanendra Veeru <gyaveeru@publicisgroupe.net>
2022-06-20 21:21:29 +0530

    kube setup

--- terraform/modules/vcn/vcn.tf
+++ terraform/modules/vcn/vcn.tf
@@ -1,6 +1,11 @@
 resource "oci_core_vcn" "test_vcn" {
-    #Required
     compartment_id = var.compartment_id
     cidr_block = var.vcn_cidr_block
     display_name = var.vcn_display_name
+}
+
+resource "oci_core_subnet" "test_subnet" {
+    cidr_block = var.subnet_cidr_block
+    compartment_id = var.compartment_id
+    vcn_id = oci_core_vcn.test_vcn.id
 }
